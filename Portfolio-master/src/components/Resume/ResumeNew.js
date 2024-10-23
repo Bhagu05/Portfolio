0d09dc38dec53e9944d34b78a7c3c8fdbf7b0e86
Keval Paresh Chheda <67334735+kevalchheda@users.noreply.github.com>
2024-10-23 16:01:44 -0400

    changes

--- Portfolio-master/src/components/Resume/ResumeNew.js
+++ Portfolio-master/src/components/Resume/ResumeNew.js
@@ -62,6 +62,7 @@ function ResumeNew() {
               file={pdf}  // Load PDF from Cloudflare
               onLoadError={onError}
               className="d-flex justify-content-center"
+              crossOrigin="anonymous" 
             >
               <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
             </Document>
